ab3f56850fa141f8a020e1c017093e3bf72cd3b9
limiter121 <33779891+limiter121@users.noreply.github.com>
2025-10-28 19:26:10 +0200

    refactor(main): use official github button

---
+++ sites/main/app/components/header/github.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import GitHubButton from "react-github-btn"
+
+export default function HeaderGithub(props) {
+	return (
+		<div className="github-btn">
+			<GitHubButton href="https://github.com/limiter121/AppwriteHub" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star limiter121/AppwriteHub on GitHub">Star</GitHubButton>
+		</div>
+	)
+}
--- sites/main/app/components/header/index.jsx
+++ sites/main/app/components/header/index.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import HeaderUser from "./user";
 import HeaderLinks from "./links";
 import { getLoggedInUser } from "@/lib/server/appwrite";
+import HeaderGithub from "./github";
 
 export default async function Header() {
   const user = await getLoggedInUser();
@@ -22,7 +23,7 @@ export default async function Header() {
           </span>
         </Link>
         <HeaderLinks authenticated={!!user} />
-        <iframe src="https://ghbtns.com/github-btn.html?user=limiter121&repo=AppwriteHub&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="120" height="30" title="GitHub" className="mt-2 md:mt-0"></iframe>
+        <HeaderGithub />
         <HeaderUser className="mt-4 md:mt-0" user={user} />
       </div>
     </header>
